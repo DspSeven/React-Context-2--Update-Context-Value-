e746fd556641084f369da92a79663c8162badcca
Your Name <you@example.com>
2023-02-10 10:24:19 +0000

    React-Context-2-(Update-Context-Value)

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {Component} from 'react'
+import LanguageContext from './context/LanguageContext'
+import Header from './components/Header'
+import LandingSection from './components/LandingSection'
+import FeaturesSection from './components/FeaturesSection'
+
+class App extends Component {
+  state = {activeLanguage: 'EN'}
+
+  changeLanguage = activeLanguage => {
+    this.setState({activeLanguage})
+  }
+
+  render() {
+    const {activeLanguage} = this.state
+    return (
+      <LanguageContext.Provider
+        value={{
+          activeLanguage,
+          changeLanguage: this.changeLanguage,
+        }}
+      >
+        <Header />
+        <LandingSection />
+        <FeaturesSection />
+      </LanguageContext.Provider>
+    )
+  }
+}
+
+export default App
